6279afd600ef925dcae680c25dc9caca019cb06a
Vivek <vivek.mitra@outlook.com>
2024-12-14 13:41:37 +1100

    made changes to pubkey create function

--- client/src/publicKeyGenerator.js
+++ client/src/publicKeyGenerator.js
@@ -1,10 +1,14 @@
+import { hexToBytes } from "ethereum-cryptography/utils";
+
 const { publicKeyCreate } = require("ethereum-cryptography/secp256k1-compat");
 const { toHex } = require("ethereum-cryptography/utils.js");
 
 export function generatePublicKey(privateKeyMapping) {
   let pubKeyMapping = {};
   for (const address in privateKeyMapping) {
-    pubKeyMapping[address] = toHex(publicKeyCreate(privateKey, false));
+    const privateKeyHex = privateKeyMapping[address];
+    const privateKeyBytes = hexToBytes(privateKeyHex);
+    pubKeyMapping[address] = toHex(publicKeyCreate(privateKeyBytes, false));
   }
   return pubKeyMapping;
 }
